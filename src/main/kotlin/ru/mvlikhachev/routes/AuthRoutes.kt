e4fc24642fa1e4e2ba5ef3983e0398c20358b19f
MV.Likhachev <m.lihachev@paysend.com>
2022-08-23 20:44:32 +0200

    Implemented getSecretInfo feature

--- src/main/kotlin/ru/mvlikhachev/routes/AuthRoutes.kt
+++ src/main/kotlin/ru/mvlikhachev/routes/AuthRoutes.kt
@@ -2,6 +2,8 @@ package ru.mvlikhachev.routes
 
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -98,5 +100,16 @@ fun Route.signIn(
                 token = token
             )
         )
+    }
+}
+
+
+fun Route.getSecretInfo() {
+    authenticate {
+        get("secret") {
+            val principal = call.principal<JWTPrincipal>()
+            val userId = principal?.getClaim("userId", String::class)
+            call.respond(HttpStatusCode.OK, "Your id: $userId")
+        }
     }
 }
